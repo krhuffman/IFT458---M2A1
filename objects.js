20de275089943ca580d8afcc5f44bcc951cf8817
Kalee Huffman <111530207+krhuffman@users.noreply.github.com>
2023-01-29 16:42:10 -0700

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,15 @@
+var students = ['John', 'Sammy', 'Jamie', 'Mike', 'Jane'];
+
+// console.log(students[0]);
+// console.log(students[3]);
+
+console.log(students.length);
+
+var grades = [90, 99, 89, 100, 86];
+
+// console.log(grades[0]);
+// console.log(grades[3]);
+
+console.log(grades.length);
+console.log(grades[grades.length - 1]);
+console.log(grades[grades.length - 2]);
